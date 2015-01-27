35f07fabfc5900ac99a6bba6bf68951e6816c000
barisser <barisser@gmail.com>
2015-01-27 00:07:30 -0800

    rehash

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import *
+
+app = Flask(__name__, static_url_path='')
+app.config['PROPAGATE_EXCEPTIONS']=True
+
+@app.route('/', methods=['GET'])
+def home():
+  return app.send_static_file('index.html')
+
+if __name__ == '__main__':
+  app.run()
